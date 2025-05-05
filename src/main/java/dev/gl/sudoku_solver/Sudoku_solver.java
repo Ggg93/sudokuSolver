adf85add70e2f9992806a742ff8054ca523a973f
gl <g.lipovtsev@gmail.com>
2025-05-05 20:44:10 +0300

    Initial commit

---
+++ src/main/java/dev/gl/sudoku_solver/Sudoku_solver.java
@@ -0,0 +1,12 @@
+package dev.gl.sudoku_solver;
+
+/**
+ *
+ * @author gl
+ */
+public class Sudoku_solver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
