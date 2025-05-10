97f0ef19573b61029ef177be43cbe80b07a4569d
gl <g.lipovtsev@gmail.com>
2025-05-10 19:59:42 +0300

    FEST library is used for gui tests. Much better!

--- src/test/java/dev/gl/sudoku_solver/gui/MainWindowTests.java
+++ src/test/java/dev/gl/sudoku_solver/gui/MainWindowTests.java
@@ -1,14 +1,16 @@
 package dev.gl.sudoku_solver.gui;
 
-import dev.gl.sudoku_solver.controllers.GoAction;
-import java.awt.AWTException;
+import org.fest.swing.edt.*;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.lang.reflect.Field;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 import javax.swing.JButton;
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import org.fest.swing.core.matcher.DialogMatcher;
+import org.fest.swing.core.matcher.JButtonMatcher;
+import org.fest.swing.fixture.DialogFixture;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JButtonFixture;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
@@ -22,19 +24,35 @@ import org.junit.platform.commons.util.ReflectionUtils;
 public class MainWindowTests {
 
     private static MainWindow mw = null;
+    private static FrameFixture frameFixture;
+
+//    @BeforeAll
+//    @Disabled
+//    @Test
+//    static void initInstance() {
+//        mw = new MainWindow();
+//        mw.setVisible(true);
+//        assertNotNull(mw, "MainWindow is not initialized");
+//    }
 
     @BeforeAll
-    @Test
-    static void initInstance() {
-        mw = new MainWindow();
-        mw.setVisible(true);
-        assertNotNull(mw, "MainWindow is not initialized");
+    static void setup() {
+        JFrame jframe = GuiActionRunner.execute(new GuiQuery<JFrame>() {
+            @Override
+            protected JFrame executeInEDT() throws Throwable {
+                return new MainWindow();
+            }
+        });
+        
+        frameFixture = new FrameFixture(jframe);
+        frameFixture.show();
     }
 
     /**
-     * Probably it will be better to use FEST Fluent Assertions
-     * because this way is too difficult and require a lot of additional code
-     * @throws Exception 
+     * Probably it will be better to use FEST Fluent Assertions because this way
+     * is too difficult and require a lot of additional code
+     *
+     * @throws Exception
      */
     @Disabled("need to use another library than just reflection API")
     @Test
@@ -42,7 +60,7 @@ public class MainWindowTests {
         Field field = ReflectionUtils.findFields(mw.getClass(), (f) -> {
             return f.getName().equals("goButton");
         }, ReflectionUtils.HierarchyTraversalMode.TOP_DOWN).get(0);
-        
+
         field.setAccessible(true);
         JButton goButton = (JButton) field.get(mw);
 
@@ -52,8 +70,13 @@ public class MainWindowTests {
         r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 //        assertNotNull(((GoAction) mw.getGoAction()).getjDialog(), "JDialog is null");
+    }
 
-
+    @Test
+    void goButtonWithoutEnoughDataShowsJOptionPane2() throws Exception {
+        frameFixture.button(JButtonMatcher.withText("Go!")).click();
+        DialogFixture dialog = frameFixture.dialog(DialogMatcher.withTitle("Sudoku Verifier"));
+        dialog.requireVisible();
     }
 
 }
